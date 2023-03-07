d93b3003ebe7c5cec83e379c54bcbcda5e5c9fed
Ángel <amartiinezg@gmail.com>
2023-03-07 19:42:47 +0100

    Solucionat problemes d'inicialització.

--- src/main/java/com/erphotel/ErphotelApplication.java
+++ src/main/java/com/erphotel/ErphotelApplication.java
@@ -1,4 +1,4 @@
-package com.erphotel.erphotel;
+package com.erphotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/erphotel/ErphotelApplicationTests.java
+++ src/test/java/com/erphotel/ErphotelApplicationTests.java
@@ -1,4 +1,4 @@
-package com.erphotel.erphotel;
+package com.erphotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
