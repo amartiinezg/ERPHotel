e2836da812b8734fa87ef95388a7b456e908dc15
Ángel <amartiinezg@gmail.com>
2023-03-21 20:55:04 +0100

    Añadidos a JavaScript objeto persona en el change listener.

--- src/main/java/com/erphotel/MainController.java
+++ src/main/java/com/erphotel/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.erphotel.Booking.domain.Book;
-import com.erphotel.Booking.service.BookService;
 import com.erphotel.personManagement.domain.PersonDomain;
 import com.erphotel.personManagement.service.PersonService;
 
