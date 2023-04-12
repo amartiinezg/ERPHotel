14496343967b42d03b46a08537a025898aa61646
Adam Sánchez Portero <adamsanchezdev@gmail.com>
2023-04-12 11:15:27 +0200

    iframe arreglado, cambios diseño en invoice.

--- src/main/java/com/erphotel/Login/controller/ConfigurationAutentificator.java
+++ src/main/java/com/erphotel/Login/controller/ConfigurationAutentificator.java
@@ -10,59 +10,55 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration //Indica al sistema que és una classe de configuració
-@EnableWebSecurity //Habilita la seguretat web
+@Configuration
+@EnableWebSecurity
 @Slf4j
 public class ConfigurationAutentificator {
 
     @Autowired
-    private UserDetailsService userDetailsService; //Objecte per recuperar l'usuari
+    private UserDetailsService userDetailsService;
 
-    /*AUTENTICACIÓ*/
- /*Injectem mitjançant @Autowired, els mètodes de la classe AuthenticationManagerBuilder. Mitjançant
-     *aquesta classe cridarem al mètode userDetailsService de la classe AuthenticationManagerBuilder què és el mètode que
-     *realitzarà l'autenticació. Per parm̀etre el sistema li passa l'usuari introduit en el formulari d'autenticació.
-     *Aquest usuari ens el retorna el mètode loadUserByUsername implementat en UsuariService.
-     *
-     *Cridem al mètode passwordEncoder passant-li com a paràmetre un objecte de tipus BCryptPasswordEncoder()
-     *per encriptar el password introduït per l'usuari en el moment d'autenticar-se i comparar-lo
-     *amb l'encriptació del password guardat a la BBDD corresponent a l'username introduït també en l'autenticació.
-     */
     @Autowired
     public void autenticacio(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
-    String[] staticResources = {
-        "/css/**",
-        "/images/**",
-        "/fonts/**",
-        "/scripts/**",
-        "/error/**"};
 
-    //L'indica al sistema que el mètode és un Bean, en aquest cas perquè crea un objecte de la classe HttpSecurity
+    String[] staticResources = {
+            "/css/**",
+            "/images/**",
+            "/fonts/**",
+            "/scripts/**",
+            "/error/**"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         return http
-                .authorizeHttpRequests((requests) -> requests
-                .requestMatchers(staticResources).permitAll()
+                .headers().frameOptions().disable().and()
 
-                .requestMatchers("/", "home", "/home/**", "/person/**", "/savePersona", "/homeScript.js", "/invoiceManager/**", "/invoice/**", "/invoiceLines/**", "/assets/**", "/homeStyle.css", "/error/**", "/functions/**",
-                        "/rooms/**", "/hotel_booking/**").hasAnyAuthority("recepcion", "limpieza", "staff")
-                .requestMatchers("/**").hasAnyAuthority("staff")
-                .anyRequest().authenticated()
+                .authorizeHttpRequests((requests) -> requests
+
+                        .requestMatchers(staticResources).permitAll()
+                        .requestMatchers("/", "home", "/home/**", "/person/**", "/savePersona", "/homeScript.js", "/invoiceManager/**", "/invoice/**", "/invoiceLines/**", "/assets/**", "/homeStyle.css", "/error/**", "/functions/**", "/rooms/**", "/hotel_booking/**").hasAnyAuthority("recepcion", "limpieza", "staff")
+                        .requestMatchers("/**").hasAnyAuthority("staff")
+                        .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll()
+                        .loginPage("/login")
+                        .permitAll()
                 )
                 .logout((logout) -> logout.
-                permitAll()
+                        permitAll()
                 )
                 .exceptionHandling((exception) -> exception
-                .accessDeniedPage("/error/error403")
+                        .accessDeniedPage("/error/error403")
                 )
                 .build();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/com/erphotel/MainController.java
+++ src/main/java/com/erphotel/MainController.java
@@ -1,6 +1,8 @@
 package com.erphotel;
 
+import com.erphotel.personManagement.domain.PersonDomain;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -17,15 +21,7 @@ public class MainController {
         return "home";
     }
 
-    @GetMapping("/hotel_booking")
-    public String bookingWidget(Model model, @AuthenticationPrincipal User username) {
-        List<PersonDomain> personas = personService.listPersonas();
-        for (PersonDomain person : personas) {
-            System.out.println(person.toString());
-        }
-        model.addAttribute("personas", personas);
-        return "hotel_booking";
-    }
+
 
     @GetMapping("/home")
     public String home(Model model, @AuthenticationPrincipal User username) {
